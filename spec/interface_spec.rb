06e82a00a838e762a99a27f6b92ac434019d340c
Patrick Shobe <patrick@patrickshobe.com>
2019-03-14 06:33:07 -0600

    Add Interface Closes #4 Closes #9

---
+++ spec/interface_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require './lib/interface'
+
+describe 'Interface Spec' do
+it 'should exist' do
+    interface = Interface.new
+    expect(interface).to be_a(Interface)
+  end
+  it 'should greet new users' do
+    interface = Interface.new
+    expect(STDOUT).to receive(:puts).with("Welcome to RPN Calculator\nPress q/Q to quit")
+    interface.welcome
+  end
+  it '#validate_digit success' do
+    interface = Interface.new
+    result = interface.validate_digit('9')
+    expect(result).to eq(9)
+  end
+  it '#validate_operator success' do
+    interface = Interface.new
+    result = interface.validate_operator('+')
+    expect(result).to eq('+')
+  end
+end
+
